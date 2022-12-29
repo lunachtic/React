27823ce771529d792aaa75c9bd5e410ccbed1fe3
Lucas Gerlach Nachtigall <lucasnachtigall@hotmail.com>
2022-12-29 18:22:04 -0300

    Project 7v0.2
    Add component files and folder
    Add navbar base
    Add navbar style

---
+++ 45-Projects/7-navbar/src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+import './Navbar.css'
+
+class Navbar extends Component {
+
+    state = {clicked:false}
+
+    handleClick = () => {
+        this.setState({clicked:!this.state.clicked})
+    }
+
+    render() {
+        return (
+            <nav className="NavbarItems" role="navigation">
+                <h1 className="navbar-logo">React <i className='fab fa-react'></i> </h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ur className={this.state.clicked ? 'nav-menu-active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>{item.title}</a>
+                            </li>
+                        );
+                    })}
+                </ur>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
